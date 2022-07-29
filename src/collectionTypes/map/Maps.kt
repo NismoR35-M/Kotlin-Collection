878053d36a323010df4a6a333a07cbeaec9075c2
Kiprono Naaman <nismoms55@gmail.com>
2022-07-29 16:19:18 +0300

    Detailed implementation of Maps.

--- src/collectionTypes/map/Maps.kt
+++ src/collectionTypes/map/Maps.kt
@@ -2,5 +2,16 @@
 package collectionTypes.map
 
 fun main() {
+    val httpHeaders = mutableMapOf(
+        "Authorization" to "api-key",
+        "contentType" to "application/json",
+        "userLocale" to "Norway"
+    )
+    //Change values by its keys
+    httpHeaders["Authorization"] = "anotherThing"
+    println(httpHeaders["Authorization"]) //anotherThing
+    println(httpHeaders)
 
+    //TODO : Iterating through Maps in kotlin is a bit special
+    httpHeaders.forEach{key, value -> println("value for key $key is $value")}
 }
