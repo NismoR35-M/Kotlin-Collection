4681b6c0732e812949c9358c0ab6f79b2e5647c6
Kiprono Naaman <nismoms55@gmail.com>
2022-07-29 15:55:45 +0300

    Detailed implementation of Sets.

--- src/collectionTypes/set/Sets.kt
+++ src/collectionTypes/set/Sets.kt
@@ -1,5 +1,12 @@
-
 package collectionTypes.set
+
+import com.sun.corba.se.spi.orbutil.threadpool.Work
+
+/*
+Set is a collection of elements where each of the elements is unique and there
+are no duplicates.Useful when you need to filter out duplicates.
+Sets are more advanced version of lists with internal filtering of data.
+*/
 
 data class Worker(
     val id: Int,
@@ -7,5 +14,15 @@ data class Worker(
 )
 
 fun main() {
-
+    val workers = mutableSetOf(
+        Worker(id = 5, name = "Philip"),
+        Worker(id = 6, name = "Watson"),
+        Worker(id = 7, name = "Vossen"),
+        Worker(id = 5, name = "Philip") //duplicate.
+    )
+    //set uses hashCode() internally to filter out duplicates.
+    println(workers) //[Worker(id=5, name=Philip), Worker(id=6, name=Watson), Worker(id=7, name=Vossen)]
+    val removedWorker = Worker(id = 5, name = "Philip")
+    workers.remove(removedWorker)
+    println(workers)
 }
