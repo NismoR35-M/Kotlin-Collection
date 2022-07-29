d77d06bbddea7d391b64d3c266b3f49626d240bd
Kiprono Naaman <nismoms55@gmail.com>
2022-07-29 14:40:21 +0300

    Detailed implementation of Arrays but not exhaustive

--- src/collectionTypes/array/Arrays.kt
+++ src/collectionTypes/array/Arrays.kt
@@ -2,5 +2,26 @@ package collectionTypes.array
 
 fun main() {
   val arrayOfNumbers = arrayOf(2, 3, 5, 6, 10)
-  println(arrayOfNumbers)
+  //iterate using forEach
+  arrayOfNumbers.forEach { number -> println(number) }
+
+  //or use for loop
+  for (number in arrayOfNumbers){
+    println(number)
+  }
+
+  //Accessing an array element
+  val firstValue = arrayOfNumbers[0]
+  println(firstValue)
+
+  //to change value of first element
+  arrayOfNumbers[0] = 38
+  val newFirstElement = arrayOfNumbers[0]
+  println(newFirstElement)
+
+  //array syntax
+  val someOtherArray = Array(5) {""}
+  println(someOtherArray)
+
+  //TODO : Check 'varags' feature.
 }
