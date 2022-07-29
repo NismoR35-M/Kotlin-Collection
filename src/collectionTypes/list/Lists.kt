ecbfcaccb7f19d5ff00a50ef0595f0afdae0304b
Kiprono Naaman <nismoms55@gmail.com>
2022-07-29 14:59:11 +0300

    Detailed implementation of List both mutable and immutable

--- src/collectionTypes/list/Lists.kt
+++ src/collectionTypes/list/Lists.kt
@@ -1,6 +1,17 @@
-
 package collectionTypes.list
 
 fun main() {
+    val list = listOf(2, 3, 6, 9, 12)
+    println(list) //prints out number bcoz all true collection overrides 'toString()'
 
+    //TODO: You can change the size and contents of Kotlin collection which starts with 'Mutable'
+    val anotherList = mutableListOf(1,2,3,4,5)
+    anotherList[2] = 200
+    println(anotherList[2]) //200
+    anotherList.add(index = 3, element = 500) //dynamic size feature of a list
+    println(anotherList[3]) //500
+    anotherList.remove(5)
+    println(anotherList) // [1, 2, 200, 500, 4]
+    anotherList.removeAt(0)
+    println(anotherList) // [2, 200, 500, 4]
 }
